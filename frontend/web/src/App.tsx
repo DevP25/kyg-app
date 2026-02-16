c47d938944e7a75220e7a3e5a8e184491169bf76
Dev Patel <dev252002@gmail.com>
2026-02-16 18:02:36 -0500

    async variable update and push for version control over devices

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -5,20 +5,53 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [selectedState, setSelectedState] = useState("");
   const [data, setData] = useState<any>(null);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/senators?state=GA')
-      .then(res => res.json())
-      .then(setData);
-  }, []);
+  const states = [
+    "AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA",
+    "HI","ID","IL","IN","IA","KS","KY","LA","ME","MD",
+    "MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ",
+    "NM","NY","NC","ND","OH","OK","OR","PA","RI","SC",
+    "SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"
+  ];
+
+  const handleChange = async (e: { target: { value: any; }; }) => {
+    const state = e.target.value;
+    setSelectedState(state);
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/senators?state=${state}`
+      );
+
+      const json = await response.json();
+      setData(json);
+    } catch(error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   return (
     <>
-      <div style={{ padding: '2rem' }}>
-        <h1>Georgia Senators</h1>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div>
+        <h1> Select a State </h1>
+        <select value={selectedState} onChange={handleChange}>
+          <option value="">-- Choose a state --</option>
+          {states.map((state) => (
+            <option key ={state} value = {state}>
+              {state}
+            </option>
+          ))}
+        </select>
+
+        {data && (
+        <pre style={{ marginTop: "20px" }}>
+          {JSON.stringify(data, null, 2)}
+        </pre>
+      )}
       </div>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -36,9 +69,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
